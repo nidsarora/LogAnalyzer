4bf505bf1ed377a7bb9140c3eee677df72ed3bf3
Nidhi Arora <aroranidhi10@gmail.com>
2017-04-14 14:00:20 -0400

    Added solutions to ques 3,5,6,7,8

--- log_analyzer.py
+++ log_analyzer.py
@@ -5,6 +5,7 @@ import glob
 from pyspark import SparkContext, SparkConf
 import re
 from pyspark.sql import SQLContext
+from collections import Counter
 
 from pyspark.sql import Row 
 conf = SparkConf().setAppName("wordcount1").setMaster("local")
@@ -14,6 +15,7 @@ sqlContext = SQLContext(sc)
 question_num=sys.argv[1]
 input_folder=sys.argv[2]
 input_folder2=sys.argv[3]
+output_file=sys.argv[4]
 
 #4.242.88.10 - - [07/Mar/2004:16:05:49 -0800] "GET /twiki/bin/edit/Main/Double_bounce_sender?topicparent=Main.ConfigurationVariables HTTP/1.1" 401 12846
 
@@ -23,6 +25,8 @@ APACHE_ACCESS_LOG_PATTERN = '^"(\S+) (\d{3}) (\S+)" (\S+) (\S+\:) (\S+) (\S+) (\
 
 def g(x):
  print x
+def h(x):
+ print x.count()
  #\[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+) (\S+)" (\d{3}) (\d+)'
 
 # Returns a dictionary containing the parts of the Apache Access Log.
@@ -67,7 +71,7 @@ def ques_2(input_folder,input_folder2):
  linesStarting2 = file2.filter(lambda s: 'Starting' in s)
  linesSession2 = linesStarting2.filter(lambda s: 'Session' in s)
  notUniqueUsers2 =linesSession2.filter(lambda s: 'achille' in s)
-  
+ return linesSession1
  print(notUniqueUsers1.count())
  print(notUniqueUsers2.count())
 
@@ -88,15 +92,16 @@ def ques_3(input_folder,input_folder2):
 
  csv_data1 = AllUsers11.map(lambda l: l.split(" "))
  row_data1 = csv_data1.map(lambda p: Row(
-    user=(p[10])
-    
+    user=(p[10]),
+    user_id=(p[7])
     )
     #Feb 27 03:50:01 iliad systemd: Starting Session 4248 of user achille
  )
  #rowdata1.distinct.collect().foreach(println)
  csv_data2 = AllUsers22.map(lambda l: l.split(" "))
  row_data2 = csv_data2.map(lambda p: Row(
-    user2=(p[10])
+    user=(p[10]),
+    user_id2=(p[7])
     
     )
     #Feb 27 03:50:01 iliad systemd: Starting Session 4248 of user achille
@@ -105,12 +110,15 @@ def ques_3(input_folder,input_folder2):
  interactions_df.registerTempTable("interactions") 
  print('hey') 
  content_size_stats = (sqlContext
-                      .sql("select distinct user from interactions where user !='user'" )).show()
+                      .sql("select distinct user from interactions where user !='user'" ))
  interactions2_df = sqlContext.createDataFrame(row_data2)
  interactions2_df.registerTempTable("interactions2") 
  print('hey') 
  content_size_stats2 = (sqlContext
-                      .sql("select distinct user2 from interactions2 where user2 !='user'" )).show()
+                      .sql("select distinct user from interactions2 where user !='user'" ))
+ 
+ print(content_size_stats.show())
+ return interactions_df, interactions2_df
  #print(content_size_stats.count())
 
  #content_size_stats.foreach(g)
@@ -119,13 +127,71 @@ def ques_3(input_folder,input_folder2):
  """
  distinctValues = df.map(lambda x : x).distinct().collect()
  distinctValues.foreach(g)"""
-    
+ 
+def ques_4(input_folder,input_folder2):
+ uniqueUsers=ques_3(input_folder,input_folder2)
+ #df1.registerTempTable("interactionsdf1") 
+ """print('hey') 
+ content_size_stats = (sqlContext
+                      .sql("select user,\
+                                    count(user_id) from interactionsdf1 \
+                                    where user !='user'\
+                                    group by user " )).show()"""
+ data=ques_2(input_folder,input_folder2)
+ for i in uniqueUsers:
+  notUniqueUsers1=data.filter(lambda s: i in s)
+  print(notUniqueUsers1.count())
+
+def ques_5(input_folder,input_folder2):
+ file=sc.textFile(input_folder)
+ file2=sc.textFile(input_folder2)
+ """linesStarting1 = file.filter(lambda s: 'Starting' in s)
+ linesSession1= linesStarting1.filter(lambda s: 'Session' in s)"""
+ errorLines1 = file.filter(lambda s: 'ERROR' in s.upper())   
+ errorLines2 = file2.filter(lambda s: 'ERROR' in s.upper()) 
+ print(errorLines1.count())
+ print(errorLines2.count())
+ return errorLines1, errorLines2                  
+ 
+"""def ques_6(input_folder,input_folder2,output_file):
+ errorLines1,errorLines2=  ques_5(input_folder,input_folder2)
+ pairs=errorLines1.map(lambda x: (x,1))
+ #counts=pairs.reduceByKey(lambda x,y: x+y)
+ #output = counts.map(lambda (k,v): (v,k)).sortByKey(True).take(5)
+ counter = Counter(pairs)
+ print(counter.most_common()[:5]) 
+ #print(output)"""
+
+def ques_7(input_folder,input_folder2):
+ table1_df, table2_df = ques_3(input_folder,input_folder2)
+ table1_df.registerTempTable("table1") 
+ table2_df.registerTempTable("table2") 
+ 
+ queryResult=(sqlContext
+                      .sql("select distinct table1.user\
+        from table1 inner join table2\
+        on table1.user = table2.user\
+        where table1.user != 'user' " )).show()  
+  
 if(question_num=='1'):
  ques_1(input_folder, input_folder2)
 
 if(question_num=='2'):
- ques_2(input_folder)
+ ques_2(input_folder,input_folder2)
 
 if(question_num=='3'):
  ques_3(input_folder,input_folder2)
-         
+
+if(question_num=='4'):
+ ques_4(input_folder,input_folder2)
+
+if(question_num=='5'):
+ ques_5(input_folder,input_folder2)
+
+if(question_num=='6'):
+ ques_6(input_folder,input_folder2,output_file)
+
+if(question_num=='7'):
+ ques_7(input_folder,input_folder2)
+
+ 
